b3d657245f5b538f154446650cf49b8ab8340451
Ronlkin <Ronlkin@noreply.gitcode.com>
2026-09-15 20:49:34 +0800

    auto update

---
+++ include/VMByteCode.h
@@ -0,0 +1,12 @@
+#ifndef VMBYTECODE_H
+#define VMBYTECODE_H
+
+#include <stdint.h>
+
+namespace vela
+{
+
+}
+
+
+#endif // VMBYTECODE_H
---
+++ include/VelaVM.h
@@ -0,0 +1,21 @@
+#ifndef VELAVM_H
+#define VELAVM_H
+
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <iterator>
+#include <stdint.h>
+
+#include "VMByteCode.h"
+
+
+
+namespace vela
+{
+
+
+}
+
+
+#endif // VELAVM_H
